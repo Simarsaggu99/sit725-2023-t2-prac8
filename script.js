9fd7ad62720545e909966dbb24e4f31c140c3854
simarsaggu <simarsaggu1999@gmail.com>
2023-08-01 11:49:08 +0530

    Bootstrapping Express app

---
+++ script.js
@@ -0,0 +1,22 @@
+const clickMe = () => {
+    alert("Thanks for clicking me. Hope you have a nice day!")
+}
+const submitForm = () => {
+    let formData = {};
+    formData.first_name = $('#first_name').val();
+    formData.last_name = $('#last_name').val();
+    formData.password = $('#password').val();
+    formData.email = $('#email').val();
+    console.log("Form Data Submitted: ", formData);
+}
+
+$(document).ready(function () {
+    $('.materialboxed').materialbox();
+    $('#formSubmit').click(() => {
+        submitForm();
+    });
+    $('#clickMe').click(() => {
+        $('#modal').modal('open');
+    });
+    $('.modal').modal();
+});
--- server.js
+++ server.js
@@ -2,14 +2,29 @@ const express = require("express")
 const app = express()
 const port = 3000;
 app.use(express.static(__dirname + "/"))
-app.get("/wordCounter", (req, res)=>{
-    const s1 = req.query.s1
-    let string = s1.replace(/\s+/g, " ").trim();
-    let wordCount = string.split(' ').length;
-    res.json({
-        message:"success",
-        result : wordCount
-    })
+app.set('view engine', 'ejs');
+const cardList = [
+    {
+        title: "IOS",
+        image: "../images/iphone.jpg",
+        link: "Iphone 13 blue color",
+        desciption: "Iphone 13 blue color"
+      },
+    {
+      title: "Android",
+      image: "../images/Samsung.jpeg",
+      link: "Samsung S20 Sky blue color",
+      desciption: "Samsung S20 Sky blue color"
+    },
+    {
+      title: "Windows",
+      image: "../images/Windows.jpeg",
+      link: "Nokia Lumia 520 Blue color",
+      desciption: "Nokia Lumia 520 Blue color"
+    }
+  ];
+app.get("/", (req, res)=>{
+    res.render("index.ejs", {cardList})
 })
 app.listen(port,()=>{
 console.log("App listening to: "+port)
